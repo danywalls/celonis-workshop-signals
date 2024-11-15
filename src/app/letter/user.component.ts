0ce75b1c816163da4e2347e3f2bb80291ae1b924
Dany Paredes <danywalls4@gmail.com>
2024-11-15 16:29:23 +0100

    first version

--- src/app/letter/user.component.ts
+++ src/app/letter/user.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output,} from '@angular/core';
+import {Component, EventEmitter, Input, Output,} from '@angular/core';
 import {User} from '../services/users.service';
 import {NgClass} from '@angular/common';
 import {getRandomTime} from '../utils/utils';
